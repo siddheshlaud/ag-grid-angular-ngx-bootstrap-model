f0b499cfacfb103de9860bb518afc3877783cbc6
siddheshlaud <sidd.laud@gmail.com>
2021-03-24 00:58:35 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ag-grid-angular-ngx-bootstrap-model
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ag-grid-angular-ngx-bootstrap-model)
---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component, TemplateRef } from "@angular/core";
+import { BsModalService, BsModalRef } from "ngx-bootstrap/modal";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
+})
+export class AppComponent {
+  modalRef: BsModalRef;
+  constructor(private modalService: BsModalService) {}
+
+  columnDefs = [{ field: "make" }, { field: "model" }, { field: "price" }];
+
+  rowData = [
+    { make: "Toyota", model: "Celica", price: 35000 },
+    { make: "Ford", model: "Mondeo", price: 32000 },
+    { make: "Porsche", model: "Boxter", price: 72000 }
+  ];
+
+  openModalWithClass(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(
+      template,
+      Object.assign({}, { class: "modal-lg" })
+    );
+  }
+}
